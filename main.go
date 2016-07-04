f0d298bab534e98fde9e5c01b0bea892ac06e7df
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-07-04 02:13:12 -0300

    server updates

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ type (
 		Name      string    `json:"name"`
 		ipaddr    string    `json:"ipaddr,omitempty"`
 		Character Character `json:"character,omitempty"`
+		Left      bool      `json:"left",omitempty"`
+		Keys      chan string
 
 		state State
 	}
@@ -36,8 +38,21 @@ type (
 	}
 
 	selectMessage struct {
-		Character string `json:"character"`
-		player    string `json:"player"`
+		Character string     `json:"character"`
+		Player    string     `json:"player"`
+		Status    string     `json:"status"`
+		Code      StatusCode `json:"code"`
+	}
+
+	connectMessage struct {
+		Status string     `json:"status"`
+		Code   StatusCode `json:"code"`
+		Left   bool       `json:"left"`
+	}
+
+	getInfoMessage struct {
+		Player playerName `json:"player"`
+		Action string     `json:"action"`
 	}
 )
 
@@ -57,15 +72,17 @@ const (
 	ESuccess StatusCode = iota + 1
 	ETooManyPlayers
 	ESelect
+	ENotReady
+	EInternal
 )
 
 func (c Character) String() string {
 	if c == Max {
-		return "Max"
+		return "max"
 	}
 
 	if c == Drax {
-		return "Drax"
+		return "drax"
 	}
 
 	return "<unknown>"
@@ -116,14 +133,14 @@ func toManyPlayers(conn net.Conn) {
 	conn.Write(msg)
 }
 
-func sendMessage(conn net.Conn, message statusMessage) error {
+func sendMessage(conn net.Conn, message interface{}) error {
 	data, err := json.Marshal(&message)
 
 	if err != nil {
 		return err
 	}
 
-	_, err = conn.Write(data)
+	_, err = conn.Write(append(data, '\n'))
 
 	fmt.Printf("Sent message: %s\n", string(data))
 
@@ -139,19 +156,70 @@ func sendSuccess(conn net.Conn) error {
 	return sendMessage(conn, connSuccess)
 }
 
-func getPlayer(conn net.Conn) (Player, error) {
+func sendNotReady(conn net.Conn) error {
+	notReady := statusMessage{
+		Status: "not ready",
+		Code:   ENotReady,
+	}
+
+	return sendMessage(conn, notReady)
+}
+
+func sendInternalError(conn net.Conn) error {
+	errInternal := statusMessage{
+		Status: "internal error",
+		Code:   EInternal,
+	}
+
+	return sendMessage(conn, errInternal)
+}
+
+func sendConnectSuccess(conn net.Conn, left bool) error {
+	connMessage := connectMessage{
+		Status: "Connected successfully",
+		Code:   ESuccess,
+		Left:   left,
+	}
+
+	return sendMessage(conn, connMessage)
+}
+
+func sendSelectedPlayer(conn net.Conn, player *Player) error {
+	selectedMsg := selectMessage{
+		Player:    player.Name,
+		Character: player.Character.String(),
+		Status:    "Other player ready",
+		Code:      ESuccess,
+	}
+
+	return sendMessage(conn, selectedMsg)
+}
+
+func readMessage(conn net.Conn) ([]byte, error) {
 	buf := make([]byte, 1024)
 
 	n, err := conn.Read(buf)
 
 	if err != nil {
-		return Player{}, err
+		return nil, err
 	}
 
 	fmt.Printf("Received: %s\n", string(buf[:n]))
 
+	return buf[:n], nil
+}
+
+func getPlayer(conn net.Conn) (Player, error) {
+	buf, err := readMessage(conn)
+
+	if err != nil {
+		return Player{}, err
+	}
+
 	var player Player
-	err = json.Unmarshal(buf[:n], &player)
+	err = json.Unmarshal(buf, &player)
+
+	player.Keys = make(chan string, 16)
 
 	if err != nil {
 		return Player{}, err
@@ -191,6 +259,38 @@ func getPlayerCharSelect(conn net.Conn) (Character, error) {
 	return 0, errors.New("invalid character choice")
 }
 
+func sendOtherPlayerKey(conn net.Conn, key string) error {
+	dataStr := `{"key": "` + key + `"}`
+
+	fmt.Printf("Sending info: %s\n", dataStr)
+
+	_, err := conn.Write([]byte(dataStr + "\n"))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getPlayerKey(conn net.Conn) (string, error) {
+	data, err := readMessage(conn)
+
+	if err != nil {
+		return "", err
+	}
+
+	var info map[string]string
+
+	err = json.Unmarshal(data, &info)
+
+	if err != nil {
+		return "", err
+	}
+
+	return info["key"], nil
+}
+
 func handleRequest(conn net.Conn, game *GameState) {
 	fmt.Printf("Connection established from %s\n", conn.RemoteAddr())
 
@@ -198,23 +298,54 @@ func handleRequest(conn net.Conn, game *GameState) {
 
 	game.Lock()
 
-	if len(game.players) >= 2 {
+	if len(game.players) == 2 {
 		game.Unlock()
-		toManyPlayers(conn)
+
+		data, err := readMessage(conn)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+			return
+		}
+
+		var dataObj getInfoMessage
+
+		err = json.Unmarshal(data, &dataObj)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+			return
+		}
+
+		player, ok := game.players[dataObj.Player]
+
+		if !ok {
+			fmt.Fprintf(os.Stderr, "ERROR: Player not found\n")
+			return
+		}
+
+		game.Lock()
+
+		var otherPlayer *Player
+
+		for _, p := range game.players {
+			if p == player {
+				continue
+			}
+
+			otherPlayer = p
+		}
+
+		game.Unlock()
+
+		runFSM(game, otherPlayer, player, conn, true)
 		return
 	}
-
-	/*err := connectionSuccess(conn)
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s", err.Error())
-		return
-	}*/
 
 	player, err := getPlayer(conn)
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
 		return
 	}
 
@@ -224,8 +355,25 @@ func handleRequest(conn net.Conn, game *GameState) {
 
 	game.players[playerName(player.Name)] = &player
 
+	if len(game.players) == 1 {
+		player.Left = true
+	} else {
+		player.Left = false
+	}
+
+	err = sendConnectSuccess(conn, player.Left)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+		return
+	}
+
 	game.Unlock()
 
+	runFSM(game, &player, nil, conn, false)
+}
+
+func runFSM(game *GameState, player *Player, otherPlayer *Player, conn net.Conn, remote bool) {
 	for {
 		switch player.state {
 		case stateSelect:
@@ -257,10 +405,87 @@ func handleRequest(conn net.Conn, game *GameState) {
 			player.state = stateWaitPlayers
 		case stateWaitPlayers:
 			fmt.Printf("Waiting other players...\n")
-			time.Sleep(3 * time.Second)
+
+			// get sync message
+			content, err := readMessage(conn)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+				sendInternalError(conn)
+				continue
+			}
+
+			fmt.Printf("Got message: %s\n", content)
+
+			game.Lock()
+
+			if len(game.players) < 2 {
+				game.Unlock()
+
+				sendNotReady(conn)
+
+				continue
+			}
+
+			for _, p := range game.players {
+				if p == player {
+					continue
+				}
+
+				otherPlayer = p
+			}
+
+			if otherPlayer.Character == 0 {
+				game.Unlock()
+
+				sendNotReady(conn)
+				continue
+			}
+
+			err = sendSelectedPlayer(conn, otherPlayer)
+
+			if err != nil {
+				game.Unlock()
+
+				fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+				continue
+			}
+
+			game.Unlock()
+
+			player.state = statePlay
 		case stateSync: // validate players and then enter the game
+			//sendStartPlay(conn
 		case statePlay:
-			// todo
+			if remote {
+				var err error
+
+				select {
+				case key := <-otherPlayer.Keys:
+					err = sendOtherPlayerKey(conn, key)
+				default:
+					err = sendOtherPlayerKey(conn, "")
+					time.Sleep(time.Millisecond * 500)
+				}
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+					return
+				}
+
+			} else {
+				key, err := getPlayerKey(conn)
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+					return
+				}
+
+				player.Keys <- key
+
+				fmt.Printf("Player %s pressed key %s\n", player.Name, key)
+			}
+
 		default:
 			fmt.Printf("Wrong state: %v\n", player.state)
 		}
